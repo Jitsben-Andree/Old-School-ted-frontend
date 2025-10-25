52a9432e1c46d8393b07341809f50324e2aec4fc
jitsbenPC <jitsbenPC@gmail.com>
2025-10-25 01:36:45 -0500

    se configuro el appcongig, para la comuniccacion  y el jwtinteceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,26 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+// 1. Importa los providers de HttpClient y el interceptor
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { jwtInterceptor } from './interceptors/jwt-interceptor';
+
+// (app.routes será creado en el siguiente paso)
+// import { routes } from './app.routes'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    
+    // (Descomenta esto cuando creemos app.routes.ts)
+    // provideRouter(routes), 
+    
+    // 2. Añade el provider para HttpClient
+    provideHttpClient(
+      // 3. Registra tu interceptor aquí
+      withInterceptors([
+        jwtInterceptor
+      ])
+    )
   ]
 };
